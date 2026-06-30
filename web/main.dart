3962cd98cd099a09512aeb3aa4397ebacbfc150e
member2vedo <krushiyukti1@gmail.com>
2026-06-30 19:29:00 +0000

    commit

--- web/main.dart
+++ web/main.dart
@@ -52,7 +52,7 @@ class _VedoWebViewState extends State<VedoWebView> {
           _handleGoogleSignInRequest();
         },
       )
-      ..firebase(NavigationDelegate(
+      ..setNavigationDelegate(NavigationDelegate(
         onPageFinished: (_) => setState(() => isLoading = false),
         onWebResourceError: (error) {
           if (error.isForMainFrame ?? true) {
